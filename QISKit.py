c5410729340f65d56987d333f137259fc9bf5d7d
MANABE <k.manabe0218@gmail.com>
2021-08-09 22:03:28 +0900

    start

---
+++ QISKit.py
@@ -0,0 +1,12 @@
+#%%
+from QISKit import *
+q = QuantumRegister(1)
+
+# %%26page
+q = QuantumRegister(1)
+c = ClassicalRegister(1)
+qc = QuantumCircuit(q,c)
+qc.measure(q,c)
+
+r = execute(qc, Aer.get_backend('qasm_simulator'), shots=100).result()
+print(r.get_counts())
