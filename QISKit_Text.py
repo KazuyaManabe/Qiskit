9637fe99c1ae763d8ca15f01c08db104bae3f918
MANABE <k.manabe0218@gmail.com>
2021-08-09 23:04:42 +0900

    図示もした

--- QISKit_Text.py
+++ QISKit_Text.py
@@ -1,13 +1,15 @@
 #%%
 from qiskit import *
+from qiskit.tools.visualization import circuit_drawer#36page
 
-# %%26page
+# %%31page
 q = QuantumRegister(1)
 c = ClassicalRegister(1)
 qc = QuantumCircuit(q,c)
+qc.x(q[0])#36page
+qc.x(q[0])#36page
 qc.measure(q,c)
 
 r = execute(qc, Aer.get_backend('qasm_simulator'), shots=100).result()
 print(r.get_counts())
-
-#%%
+circuit_drawer(qc)
