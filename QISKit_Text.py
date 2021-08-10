eafad1e326099dd10da69a6cbc9f6a0e0a0490f4
MANABE Kazuya <k.manabe0218@gmail.com>
2021-08-10 13:53:45 +0900

    量子回路表示

--- QISKit_Text.py
+++ QISKit_Text.py
@@ -1,15 +1,22 @@
 #%%
 from qiskit import *
-from qiskit.tools.visualization import circuit_drawer#36page
+from qiskit.tools.visualization import *
 
-# %%31page
-q = QuantumRegister(1)
-c = ClassicalRegister(1)
-qc = QuantumCircuit(q,c)
-qc.x(q[0])#36page
-qc.x(q[0])#36page
+# %%26page
+q = QuantumRegister(1) #1つの量子レジスタqの生成
+c = ClassicalRegister(1) #1つの古典的レジスタcの生成
+qc = QuantumCircuit(q,c) #量子回路qcの生成
+qc.x(q[0])#量子ビットq[0]のビット反転演算,29page
+qc.z(q[0])#量子ビットq[0]のビット反転演算,33page
 qc.measure(q,c)
 
 r = execute(qc, Aer.get_backend('qasm_simulator'), shots=100).result()
+#量子回路を実行し、結果rに代入する
 print(r.get_counts())
+#量子回路名cnの量子プログラムの実行結果rからカウント結果を取得し表示する
+rc = r.get_counts()
+print(rc)
+#量子回路名cnの量子プログラム実行によるヒストグラムデータ結果を表示する
+plot_histogram(rc)
 circuit_drawer(qc)
+# %%
